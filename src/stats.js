5f00bd74423e60f0900ea97c2cbae96871ed52b2
Chris Watson <cwatson1988@gmail.com>
2018-01-01 23:59:19 +0000

    access control headers added in

--- src/stats.js
+++ src/stats.js
@@ -2,7 +2,10 @@
 import axios from 'axios';
 
 const config = {
-  headers: { 'trn-api-key': '61fae056-d1f4-4ed2-b20f-39dff98c10a7' },
+  headers: {
+    'trn-api-key': '61fae056-d1f4-4ed2-b20f-39dff98c10a7',
+    'Access-Control-Allow-Origin': '*',
+  },
 };
 
 const wins = axios.get('https://api.fortnitetracker.com/v1/profile/xbl/bg%20watson', config)
